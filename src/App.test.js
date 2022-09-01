f9f3018aa0e9726be7259e3a471042658668ecdc
mengyuhang4879 <13784203017@163.com>
2022-09-01 16:43:50 +0800

    'test'

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 test('renders learn react link', () => {
     render(<App />);
+
     const linkElement = screen.getByText(/learn react/i);
 
     expect(linkElement).toBeInTheDocument();
